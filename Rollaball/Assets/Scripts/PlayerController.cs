1d7813a18a03a4591db2c21b219f5245a1868164
Luca Mizrahi <lucam1@al.insper.edu.br>
2024-09-15 22:15:38 -0300

    avançando no tutorial

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     // Speed of the player
     public float speed = 0;
 
+    // Text component
+    public TextMeshProUGUI scoreText;
+
+    // Win text component
+    public GameObject winTextObject;
+
     // Rigidbody component
     private Rigidbody rb;
+
+    // Score variable
+    private int score;
 
     // Movement variables along X and Y axis
     private float movementX;
@@ -20,6 +30,13 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        score = 0;
+
+        // Set the score text to 0
+        SetScoreText();
+
+        // Set the win text to inactive
+        winTextObject.SetActive(false);
     }
 
     // Functon to move the player when a move input is detected
@@ -31,6 +48,18 @@ public class PlayerController : MonoBehaviour
         // Store the X and Y components of the movement vector
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    // Function to update the score text
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+
+        // If the score is 12 or more, display the win text
+        if (score >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     void FixedUpdate() 
@@ -47,6 +76,9 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            score = score + 1;
+
+            SetScoreText();
         }
     }
 }
